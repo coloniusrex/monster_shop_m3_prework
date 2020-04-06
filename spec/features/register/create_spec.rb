1dc477a4041572f3229faf05d5da4ad0aac39897
David Tran <davidttemail@gmail.com>
2020-04-06 15:38:34 -0600

    Write tests for finding existing emails

--- spec/features/register/create_spec.rb
+++ spec/features/register/create_spec.rb
@@ -35,4 +35,28 @@ describe "registration" do
 
     expect(page).to have_content("Unable to create account: Required information missing.")
   end
+
+  xit "can't create a new user if user with email already exists" do
+    User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", confirm_pass: "password")
+
+    visit "/register"
+
+    fill_in :name, with: "David"
+    fill_in :address, with: "123 Test St"
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "80204"
+    fill_in :email, with: "123@example.com"
+    fill_in :password, with: "password"
+    fill_in :confirm_pass, with: "password"
+
+    click_on "Submit"
+    # save_and_open_page
+
+    expect(page).to have_content("Email already in use. Try a different email.")
+    expect(page).to have_content("David")
+    expect(page).to have_content("123 Test St")
+    expect(page).to have_no_content("password")
+    expect(page).to have_no_content("123@example.com")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ describe User do
       it { should validate_presence_of field }
     end
   end
+
+  describe "class methods" do
+    describe "existing_email?" do
+      it "returns true/false if a user with submitted email already exists" do
+
+        user_1 = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", confirm_pass: "password")
+        params = Hash.new
+        params[:email] = "123@example.com"
+
+        expect(User.existing_email?(params[:email])).to eq(true)
+
+      end
+    end
+  end
 end
