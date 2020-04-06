0bd6cd09f271cdf931b8ea017f72ad3e0ad3a215
David Tran <davidttemail@gmail.com>
2020-04-06 15:39:02 -0600

    Add MVC for new user failing with existing email WIP

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -5,12 +5,20 @@ class RegisterController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if User.existing_email?(params[:email])
+      @name = params[:name]
+      @address = params[:address]
+      @state = params[:state]
+      @city = params[:city]
+      @zip = params[:zip]
+      flash[:error] = "Email already in use. Try a different email."
+      redirect_to "/register"
+    elsif @user.save
       flash[:notice] = "Your account has been created."
       redirect_to "/profile"
     else
       flash[:error] = "Unable to create account: Required information missing."
-      render :new
+      redirect_to request.referer
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :email, :password, :confirm_pass
 
+  def self.existing_email?(email)
+    User.exists?(email: email)
+  end
 end
