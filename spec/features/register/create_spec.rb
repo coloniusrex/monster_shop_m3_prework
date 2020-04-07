0eb6c2a3d4b5146768cc33b9b00fc13f884e04b9
Colin Alexander <colintalex@gmail.com>
2020-04-06 19:21:58 -0600

    Makes changes for more robust testing

--- spec/features/register/create_spec.rb
+++ spec/features/register/create_spec.rb
@@ -26,17 +26,18 @@ describe "registration" do
     fill_in :name, with: "David"
     fill_in :address, with: "123 Test St"
     fill_in :city, with: "Denver"
-    fill_in :state, with: "CO"
-    fill_in :zip, with: "80204"
+    fill_in :state, with: ""
+    fill_in :zip, with: ""
     fill_in :password, with: "password"
     fill_in :confirm_pass, with: "password"
 
     click_on "Submit"
 
     expect(page).to have_content("Unable to create account: Required information missing.")
+
   end
 
-  xit "can't create a new user if user with email already exists" do
+  it "can't create a new user if user with email already exists" do
     User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", confirm_pass: "password")
 
     visit "/register"
@@ -51,12 +52,13 @@ describe "registration" do
     fill_in :confirm_pass, with: "password"
 
     click_on "Submit"
-    # save_and_open_page
 
     expect(page).to have_content("Email already in use. Try a different email.")
-    expect(page).to have_content("David")
-    expect(page).to have_content("123 Test St")
-    expect(page).to have_no_content("password")
-    expect(page).to have_no_content("123@example.com")
+    expect(find_field('Name').value).to eql "David"
+    expect(find_field('Address').value).to eql "123 Test St"
+    expect(find_field('City').value).to eql "Denver"
+    expect(find_field('State').value).to eql "CO"
+    expect(find_field('Zip').value).to eql "80204"
+    expect(find_field('Email').value).to eql "123@example.com"
   end
 end
