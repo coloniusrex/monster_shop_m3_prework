d6cbe1f72cb27740fc4bcdbc058dede446b88100
Colin Alexander <colintalex@gmail.com>
2020-04-06 19:21:32 -0600

    Adds params to partial-render locals for new form auto complete

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -5,14 +5,9 @@ class RegisterController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if User.existing_email?(params[:email])
-      @name = params[:name]
-      @address = params[:address]
-      @state = params[:state]
-      @city = params[:city]
-      @zip = params[:zip]
+    if User.exists?(email: user_params[:email])
       flash[:error] = "Email already in use. Try a different email."
-      redirect_to "/register"
+      render :new
     elsif @user.save
       flash[:notice] = "Your account has been created."
       redirect_to "/profile"
