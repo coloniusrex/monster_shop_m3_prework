627c3ae748eff027786134a62ee95c8c68484a0d
Colin Alexander <colintalex@gmail.com>
2020-04-07 15:32:17 -0600

    Creates Story 3 test

---
+++ spec/features/users/navigation_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Site Navigation' do
+  describe 'As a User' do
+    it "I see the visitor links on the nav bar plus links for a registered user" do
+      user = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/'
+      within 'nav' do
+        expect(page).to have_link('Profile')
+        expect(page).to have_link('Logout')
+        expect(page).to have_no_link('Log In')
+        expect(page).to have_no_link('Register')
+        expect(page).to have_content("Logged in as: #{user.name}")
+      end
+    end
+  end
+end
