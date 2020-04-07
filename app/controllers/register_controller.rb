d07d7a6d57e972209432201c0006b12829a8afa2
Maxwell Baird <bairdm71@gmail.com>
2020-04-07 16:02:31 -0600

    pulled from git

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -5,18 +5,19 @@ class RegisterController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if User.exists?(email: user_params[:email])
       flash[:error] = "Email already in use. Try a different email."
       render :new
-    elsif params[:confirm_pass] != params[:password]
-      flash[:error] = "Unable to create account: Passwords don't match."
+    elsif params[:confirm_pass] == params[:password]
+      flash[:error] = "Passwords don't match."
       render :new
-    elsif params[:confirm_pass] == params[:password] && @user.save
+    elsif @user.save
       flash[:notice] = "Your account has been created."
       redirect_to "/profile"
     else
       flash[:error] = "Unable to create account: Required information missing."
-      render :new
+      redirect_to request.referer
     end
   end
 
@@ -25,5 +26,4 @@ class RegisterController < ApplicationController
   def user_params
     params.permit(:name, :address, :city, :state, :zip, :email, :password)
   end
-
 end
