8288c85d14442f4f9bd5a3d23873742ba19bf4d8
Maxwell Baird <bairdm71@gmail.com>
2020-04-08 14:57:12 -0600

    created the change password method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def password
+  end
+
+  def update_password
+    user = User.find(current_user.id)
+    user.password = params[:password]
+    if user.save
+      flash[:notice] = 'Your password has been updated.'
+      redirect_to '/profile'
+    else
+      flash[:error] = 'Something went wrong'
+    end
+
+  end
 end
--- spec/features/profile/edit_spec.rb
+++ spec/features/profile/edit_spec.rb
@@ -4,23 +4,52 @@ RSpec.describe 'Profile' do
   describe 'As a User' do
     it "I edit my password" do
       user = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", role: 1)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/login'
+
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_on 'Login'
+
       visit '/profile'
       click_link 'Change password'
 
       fill_in 'password', with:'new_password'
-      fill_in 'confirm password', with: 'new_password'
+      fill_in 'confirm_password', with: 'new_password'
       click_on 'Submit'
 
       expect(current_path).to eq('/profile')
       expect(page).to have_content("Your password has been updated.")
       click_on 'Logout'
+
+      expect(current_path).to eq('/')
+      expect(page).to have_content("You're logged out")
+
+      visit '/login'
+
+      fill_in 'email', with: user.email
+      fill_in 'password', with: 'new_password'
+      click_on 'Login'
+      expect(current_path).to eq('/profile')
+    end
+
+    it "I need to have matching passwords" do
+      user = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", role: 1)
       visit '/login'
 
       fill_in :email, with: user.email
-      fill_in :password, with: 'new_password'
+      fill_in :password, with: user.password
       click_on 'Login'
-      expect(current_path).to eq('/profile')
+
+      visit '/profile'
+      click_link 'Change password'
+
+      fill_in 'password', with:'new_password'
+      fill_in 'confirm_password', with: 'different_password'
+      click_on 'Submit'
+
+
+      expect(page).to have_content("Your password and confirm password do not match.")
+
     end
   end
 end
