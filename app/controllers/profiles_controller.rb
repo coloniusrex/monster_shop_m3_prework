cab9f1792b07664cbe2c6d5a30d9790052734020
David Tran <davidttemail@gmail.com>
2020-04-07 18:13:37 -0600

    Update MVC for visitors accessing paths

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,13 @@
 class ProfilesController < ApplicationController
+  before_action :require_user
 
   def show
+  end
+
+  private
+
+  def require_user
+    render file: "public/404" unless current_user.role > 0
   end
 
 end
