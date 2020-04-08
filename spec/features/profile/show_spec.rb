9d4f3517894ccb5949aa001433de6451607ae42a
Maxwell Baird <bairdm71@gmail.com>
2020-04-08 13:56:21 -0600

    Create a profile page for user

--- spec/features/profile/show_spec.rb
+++ spec/features/profile/show_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'Profile' do
       user = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", role: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit '/profile'
-      within 'profile' do
+      within '#profile' do
         expect(page).to have_content("Username: #{user.name}")
         expect(page).to have_content("Address: #{user.address}")
         expect(page).to have_content("City: #{user.city}")
         expect(page).to have_content("State: #{user.state}")
-        expect(page).to have_content("Zipcode: #{user.zipe}")
+        expect(page).to have_content("Zipcode: #{user.zip}")
         expect(page).to have_content("Email: #{user.email}")
       end
     end
