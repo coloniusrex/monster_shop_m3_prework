a58b3c4d56f1667286590b8dc726b34917002cc5
Maxwell Baird <bairdm71@gmail.com>
2020-04-08 15:03:50 -0600

    changed my test in order to use a before each

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def update_password
     user = User.find(current_user.id)
-    if params[:password] == params[:confirm_password]
+    if params[:password] == params[:confirmation]
       user.password = params[:password]
       if user.save &&
         flash[:notice] = 'Your password has been updated.'
--- spec/features/profile/edit_spec.rb
+++ spec/features/profile/edit_spec.rb
@@ -2,19 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Profile' do
   describe 'As a User' do
-    it "I edit my password" do
-      user = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", role: 1)
+
+    before :each do
+      @user = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", role: 1)
       visit '/login'
 
-      fill_in :email, with: user.email
-      fill_in :password, with: user.password
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
       click_on 'Login'
+    end
+
+    it "I edit my password" do
 
       visit '/profile'
       click_link 'Change password'
 
       fill_in 'password', with:'new_password'
-      fill_in 'confirm_password', with: 'new_password'
+      fill_in 'confirmation', with: 'new_password'
       click_on 'Submit'
 
       expect(current_path).to eq('/profile')
@@ -26,25 +30,19 @@ RSpec.describe 'Profile' do
 
       visit '/login'
 
-      fill_in 'email', with: user.email
+      fill_in 'email', with: @user.email
       fill_in 'password', with: 'new_password'
       click_on 'Login'
       expect(current_path).to eq('/profile')
     end
 
     it "I need to have matching passwords" do
-      user = User.create(name: "David", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "123@example.com", password: "password", role: 1)
-      visit '/login'
-
-      fill_in :email, with: user.email
-      fill_in :password, with: user.password
-      click_on 'Login'
 
       visit '/profile'
       click_link 'Change password'
 
       fill_in 'password', with:'new_password'
-      fill_in 'confirm_password', with: 'different_password'
+      fill_in 'confirmation', with: 'different_password'
       click_on 'Submit'
 
 
