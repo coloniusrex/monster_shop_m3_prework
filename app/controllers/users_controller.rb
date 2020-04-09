2ab8c035d370d3c2221bee8e55547e330eaffc29
Maxwell Baird <bairdm71@gmail.com>
2020-04-09 10:10:34 -0600

    added a check to see if entered password is blank

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,15 @@ class UsersController < ApplicationController
     user = User.find(current_user.id)
     if params[:password] == params[:confirmation]
       user.password = params[:password]
-      if user.save 
+      if params[:password] == ''
+        flash[:error] = 'Your password or confirm password is blank.'
+        render :password
+      elsif user.save
         flash[:notice] = 'Your password has been updated.'
         redirect_to '/profile'
+      else
+        flash[:notice] = 'Your password did not save correctly'
+        render :password
       end
     else
       flash[:error] = 'Your password and confirm password do not match.'
