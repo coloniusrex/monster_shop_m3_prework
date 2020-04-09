ae11ea7f201185c0fa5ae66a44a2938c64dcf3d4
David Tran <davidttemail@gmail.com>
2020-04-09 10:09:48 -0600

    Move updating user info from profiles to user_controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,30 +4,7 @@ class ProfilesController < ApplicationController
   def show
   end
 
-  def edit
-  end
-
-  def update
-    if !existing_email?
-      if current_user.update!(user_params)
-        flash[:notice] = "Your information has successfully been updated."
-        redirect_to "/profile"
-      end
-    else
-      flash[:error] = "That email is already in use."
-      render :edit
-    end
-  end
-
   private
-
-  def existing_email?
-    User.exists?(email: params[:email]) && params[:email] != current_user.email
-  end
-
-  def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email)
-  end
 
   def require_user
     render file: "public/404" unless current_user.role > 0
