ef5af93827a9d0275d9215b1b0ffc22b756abae3
Maxwell Baird <bairdm71@gmail.com>
2020-04-13 13:50:40 -0600

    removed active reference from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', c
 temp_shop = Merchant.create(name: "This Shop Should Have No Items", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
 #temp_shop items
-temp = temp_shop.items.create(name: "Temp", description: "Temp", price: 55, image: "https://c-static.smartphoto.com/structured/repositoryimage/productcategory/fun_ideas/magic_mug/material/0002/image/material.jpg", inventory: 0, active?: false)
+temp = temp_shop.items.create(name: "Temp", description: "Temp", price: 55, image: "https://c-static.smartphoto.com/structured/repositoryimage/productcategory/fun_ideas/magic_mug/material/0002/image/material.jpg", inventory: 0, status: false)
 
 #tires_shop items
 tire = tires_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 75, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 20)
@@ -30,7 +30,7 @@ seat = bike_shop.items.create(name: "Bike Seat", description: "Squishy tushy.", 
 
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 25)
-dog_bone = dog_shop.items.create(name: "Dog Bone", description: "Bonified fun!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?: false, inventory: 30)
+dog_bone = dog_shop.items.create(name: "Dog Bone", description: "Bonified fun!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", status: false, inventory: 30)
 chew_rope = dog_shop.items.create(name: "Chew Rope", description: "Laffy Lasso", price: 32, image: "https://ae01.alicdn.com/kf/HTB1G1JIMXXXXXa8XVXXq6xXFXXXv/Free-Shipping-Cotton-Rope-Pet-Dog-toy-Multicolor-For-Puppy-Double-Knot-Chew-Toy-Durable-Braided.jpg", inventory: 54)
 squeaky_toy = dog_shop.items.create(name: "Squeaky Toy", description: "Dogs love it. Humans hate it.", price: 9, image: "https://i.pinimg.com/originals/90/7e/13/907e138fc1e8401dcedabb795d07fd67.jpg", inventory: 32)
 kong_toy = dog_shop.items.create(name: "Kong Toy", description: "They paid us to say that.", price: 26, image: "https://www.dogtuff.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/k/o/kong_classic_red_xxl_pkg.jpg", inventory: 50)
