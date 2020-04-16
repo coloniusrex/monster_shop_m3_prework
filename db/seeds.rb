12496921e97229bd42f561e25229fd6a06a0b56f
David Tran <davidttemail@gmail.com>
2020-04-16 11:19:19 -0600

    Update base users passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,10 +36,10 @@ squeaky_toy = dog_shop.items.create(name: "Squeaky Toy", description: "Dogs love
 kong_toy = dog_shop.items.create(name: "Kong Toy", description: "They paid us to say that.", price: 26, image: "https://www.dogtuff.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/k/o/kong_classic_red_xxl_pkg.jpg", inventory: 50)
 
 #users
-user = User.create(name: "User(Colin)", address: "123 Test St", city: "New York", state: "NY", zip: "80204", email: "user@example.com", password: "user", role: 1)
+user = User.create(name: "User(Colin)", address: "123 Test St", city: "New York", state: "NY", zip: "80204", email: "user@example.com", password: "password_regular", role: 1)
 user_2 = User.create(name: "User(David)", address: "523 Test St", city: "Colorado Springs", state: "CO", zip: "80020", email: "user1@example.com", password: "user", role: 1)
-merchant = User.create(name: "Merchant(Max)", address: "123 Test St", city: "Houston", state: "TX", zip: "80204", email: "merchant@example.com", password: "merchant", role: 2)
-admin = User.create(name: "Admin(Kevin)", address: "123 Test St", city: "Fable", state: "WA", zip: "80204", email: "admin@example.com", password: "admin", role: 3)
+merchant = User.create(name: "Merchant(Max)", address: "123 Test St", city: "Houston", state: "TX", zip: "80204", email: "merchant@example.com", password: "password_merchant", role: 2)
+admin = User.create(name: "Admin(Kevin)", address: "123 Test St", city: "Fable", state: "WA", zip: "80204", email: "admin@example.com", password: "password_admin", role: 3)
 admin_2 = User.create(name: "Admin(Louis)", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "admin2@example.com", password: "admin", role: 3)
 
 merchant_1 = User.create(name: "Employee(Billy)", address: "123 Test St", city: "Denver", state: "CO", zip: "80204", email: "merchant1@example.com", password: "merchant", role: 2)
@@ -52,7 +52,7 @@ bike_shop.add_employee(merchant)
 bike_shop.add_employee(merchant_1)
 dog_shop.add_employee(merchant_2)
 misc_shop.add_employee(merchant_3)
-tire_shop.add_employee(merchant_4)
+tires_shop.add_employee(merchant_4)
 
 #orders
 order1 = user.orders.create(id: 3, name: "Colin", address: "400 Wash", city: "Denver", state: "CO", zip: 80203)
@@ -69,7 +69,7 @@ order2 = user.orders.create(id: 4, name: "David", address: "2931 Carrier St", ci
 order2.item_orders.create(order_id: order2.id, item: pull_toy, quantity: 1, price: pull_toy.price)
 order2.item_orders.create(order_id: order2.id, item: dog_bone, quantity: 2, price: dog_bone.price)
 order2.item_orders.create(order_id: order2.id, item: rim, quantity: 4, price: rim.price)
-order2.item_orders.create(order_id: order2.id, item: chew_rope, quantity: 5, price: chew_rope.price)
+order2.item_orders.create(order_id: order2.id, item: axel, quantity: 5, price: axel.price)
 
 order3 = merchant_1.orders.create(id: 5, name: "Max", address: "123 Martin", city: "Colorado Springs", state: "CO", zip: 80203, status: "Packaged")
 
